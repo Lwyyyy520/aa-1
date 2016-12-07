5f373645b73066cec823e1fa5968ddd8fa6bb0ab
Lwyyyy520 <1209109747@qq.com>
2016-12-07 11:35:08 +0800

    123
    12345 口令

--- src/com/example/ddd/MainActivity.java
+++ src/com/example/ddd/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //12345
+        //天王盖地虎
     }
 
 
